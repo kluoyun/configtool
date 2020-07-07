cbcf46375cc4fdd5d022d480d5d1633575b18764
Jay <github@jayuk.org>
2020-07-07 11:57:40 +0100

    remove diag pin SKR 1.4

--- src/store/Boards.js
+++ src/store/Boards.js
@@ -210,7 +210,7 @@ export default {
                     panelButtonPin: "NoPin",
                     spiChannel: 2, //Software SPI
                 },
-                diagnosticPin: "1.18",
+                diagnosticPin: "NoPin",
           }
       },
 	  {

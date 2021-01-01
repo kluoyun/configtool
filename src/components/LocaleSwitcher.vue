0804bec538a5d4e971ee6551b2e8a983b705b7a7
Jay <github@jayuk.org>
2021-01-01 13:45:28 +0000

    add chinese

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -2,7 +2,8 @@
   <div class="locale-switcher">
     <select v-model="$i18n.locale">
       <option value="en">English</option>
-      <option value="pt">Português</option>
+      <option value="zh">中文</option>
+      <option value="pt">Português</option> 
     </select>
   </div>
 </template>

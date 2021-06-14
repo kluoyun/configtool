95dd8f5e84e35e54db93efefd70261a64374045d
Jay <github@jayuk.org>
2021-06-14 06:37:26 +0100

    3.2.1+15 - Added German

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -3,6 +3,7 @@
     <select v-model="$i18n.locale">
       <option value="en">English</option>
       <option value="fr">Français</option>
+      <option value="de">Deutsche</option>
       <option value="pt">Português</option>
       <option value="zh">中文</option>
     </select>

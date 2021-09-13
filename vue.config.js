be52ba0d663f1457e38b0be5f4165257b373e315
kluoyun <31061246+kluoyun@users.noreply.github.com>
2021-09-13 10:10:36 +0800

    修复JsZip无法压缩的问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-
-	publicPath: process.env.NODE_ENV === 'production' ? '/Configurator/' : '/',
+        publicPath: '../',
+	//publicPath: process.env.NODE_ENV === 'production' ? '/Configurator/' : '/',
 	chainWebpack: config => {
 		config
 			.plugin('copy')
